d8fce587c69ff5efa74c9e4153bc07ee34552aa5
RajaMoparthi <raja.git.jenkins@gmail.com>
2014-06-28 13:10:20 +0530

    added one project

---
+++ eventTest_1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace eventTest_1
+{
+    enum TTT
+    {
+        OxF2,
+        oxff
+    }
+    public partial class Form1 : Form
+    {
+        MyTestClass obj = new MyTestClass();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            obj.TestEevnt += TestDel;
+            TTT a = TTT.OxF2;
+            
+        }
+
+        private void TestDel()
+        {
+            label1.Text += "My method " + "\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            obj.RaiseEvent();
+        }
+    }
+}
